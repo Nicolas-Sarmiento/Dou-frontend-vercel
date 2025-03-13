2aa74a34e93faf23e1a168787790e1890bd0baed
JoseSLK <jose.salamanca05@uptc.edu.co>
2025-03-13 18:06:04 -0500

    Transition strip

--- src/components/header/Menu.jsx
+++ src/components/header/Menu.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../header/header.css";
 import { useState } from "react";
 
 export function LinkModules() {
     const [showOptions, setShowOptions] = useState(false);
+    const [isDropdownVisible, setIsDropdownVisibleOpen] = useState(false);
+
+    useEffect( () => {
+        if (showOptions) {
+            setIsDropdownVisibleOpen(true);
+        } else {
+            const timer = setTimeout(() => {
+                setIsDropdownVisibleOpen(false);
+            }, 300);
+            return () => clearTimeout(timer);
+        }
+    }, [showOptions]);
+
     return (
         <div className="dou-strip">
             <div
@@ -14,8 +27,8 @@ export function LinkModules() {
                 <div className="dou-menu-line mid"></div>
                 <div className="dou-menu-line"></div>
             </div>
-            {showOptions && (
-                <div className="dou-strip-options">
+            {isDropdownVisible && (
+                <div className={`dou-strip-options ${showOptions? "open": ""}`}>
                     <button className="dou-strip-option">Contenido</button>
                     <button className="dou-strip-option">Contest</button>
                     <button className="dou-strip-option">Ejercicios</button>
