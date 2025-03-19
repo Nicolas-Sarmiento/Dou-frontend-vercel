b89a25758e90fbf774a52fa1d3f2a987bf10a5bc
JoseSLK <jose.salamanca05@uptc.edu.co>
2025-03-19 18:08:35 -0500

    Add component Education-content

--- src/components/Education_content/index.jsx
+++ src/components/Education_content/index.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import "./educationContent.css";
 
 export function EducationContent () {
     return (
-        <div>
-            <h1>Contenido de Educación</h1>
+        <div className="dou-education-content">
+            <nav className="dou-menu-content">
+                <input type="text" className="dou-login-input search" />
+            </nav>
+            <div className="dou-content">
+                Algo
+            </div>
         </div>
     );
 }
